14be0499e7c2a95eee6e820ef89d27357c9fb201
MariaClaraValotti <email do usurio>
2024-02-27 19:19:34 -0300

    projeto react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Texto from "./Componentes/Texto/texto.jsx"
+import "./App.css"
+function App() {
+  return(
+  <>
+  <Texto/>
+  </>
+  )
+}
+export default App
---
+++ src/Componentes/Texto/texto.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+function texto () {
+    return (
+        <>
+        <html>
+        <h1>Direitos Fundamentais </h1>
+            <h2>Direitos fundamentais surgiu na revoluçao francesa.
+                Sao divididos em Primeira, Segunda e Terceiras Dimensoes. Os Direitos Fundamentais Mundiais surgiram em 1988 apos a segunda Guerra Mundial, surgindo tambem o tratato de versalhes criando a ONU.
+
+            </h2>
+            <ul>
+                <li>Primeira Dimensao :Direito a Liberdade e a nao participaçao do Estado na economia </li>
+                <li>Segunda Dimensao :Igualdade,Liberdade.</li>
+                <li>Terceira Dimensao:Fraternidade.</li>
+            </ul>
+            </html>
+        </>
+
+    )
+}
+export default texto
